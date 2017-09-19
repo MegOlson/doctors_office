fb1cce8b4981b00bf74fee1ff27ba5dd17888c82
Kristen Kulha and Megan Olson <megan@>
2017-09-19 10:16:06 -0700

    add method to save patients

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,9 @@
 class Patient
-  attr_reader(:name, :doctor_id, :birthday)
+  attr_reader(:name, :id, :birthday)
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    @doctor_id = attributes.fetch(:doctor_id)
+    @id = attributes.fetch(:id)
     @birthday = attributes.fetch(:birthday)
   end
 
@@ -12,10 +12,19 @@ class Patient
     patients = []
     returned_patients.each() do |patient|
       name = patient.fetch("name")
-      doctor_id = patient.fetch("id").to_i
+      id = patient.fetch("id").to_i
       birthday = patient.fetch("birthday")
-      patients.push(Patient.new({:name => name, :doctor_id => doctor_id, :birthday => birthday}))
+      patients.push(Patient.new({:name => name, :id => id, :birthday => birthday}))
     end
     patients
+  end
+
+  def ==(another_patient)
+    self.name().==(another_patient.name()).&(self.id().==(another_patient.id()))
+  end
+
+  def save
+    result = DB.exec("INSERT INTO patients (name, birthday) VALUES ('#{@name}', '#{@birthday}') RETURNING id ;")
+    @id = result.first().fetch("id").to_i
   end
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe("Patient") do
-  let(:patient) {Patient.new({:name => "Suzie", :doctor_id => nil, :birthday => "2004-04-20"})}
+  let(:patient) {Patient.new({:name => "Suzie", :id => nil, :birthday => "2004-04-20"})}
   it("creates instance of patient with Patient class when given name and birthday") do
     expect(patient.name).to eq ("Suzie")
   end
@@ -11,11 +11,19 @@ describe("Patient") do
       expect(Patient.all()).to(eq([]))
     end
   end
-  #
-  # describe("#save") do
-  #   it("saves each doctor and details to database") do
-  #     doctor.save()
-  #     expect(Doctor.all()).to(eq([doctor]))
-  #   end
-  # end
+
+  describe("#save") do
+    it("saves each patient and details to database") do
+      patient.save()
+      expect(Patient.all()).to(eq([patient]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same patient if they have the same name and doctor id") do
+      patient1 = Patient.new({:name => "Suzie", :id => nil, :birthday => "2004-04-20"})
+      patient2 = Patient.new({:name => "Suzie", :id => nil, :birthday => "2004-04-20"})
+      expect(patient1).to(eq(patient2))
+    end
+  end
 end
